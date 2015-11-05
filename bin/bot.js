9f0eba7ff07285364de7a3eb2d8a6566832d9f43
unknown <racshankar@CAGTAW203452.ca.deloitte.com>
2015-11-05 17:19:56 -0500

    working simple bot

---
+++ bin/bot.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var RacBot = require('../lib/racbot');
+
+var token = 'xoxb-13987087606-IhQracN8Dqok9Sh3N9hJHpM8'; //process.env.BOT_API_KEY;
+var dbpath = process.env.BOT_DB_PATH;
+var name = process.env.BOT_NAME;
+
+var racbot = new RacBot({
+	token: token
+	//dbPath: dbPath,
+	//name: name
+	//console.log('initializing....');
+	
+});
+
+console.log('running the bot.....');
+racbot.run();
