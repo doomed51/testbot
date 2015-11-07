e629bee8a2d3f39fac6fcce11971830d59ea3a8f
doomed51 <doomed51@gmail.com>
2015-11-07 11:25:08 -0500

    removed sql dependencies, runs locally

--- bin/bot.js
+++ bin/bot.js
@@ -3,7 +3,7 @@
 var RacBot = require('../lib/racbot');
 
 var token = 'xoxb-13987087606-IhQracN8Dqok9Sh3N9hJHpM8'; //process.env.BOT_API_KEY;
-var dbpath = process.env.BOT_DB_PATH;
+// var dbpath = process.env.BOT_DB_PATH;
 var name = process.env.BOT_NAME;
 
 var racbot = new RacBot({
